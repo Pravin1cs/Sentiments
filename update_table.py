ff04195f470e749fd6723c345462e07de7a4c5f3
Pravin1cs <pravin1.cs@gmail.com>
2025-04-18 23:29:43 +0530

    Add files via upload

---
+++ update_table.py
@@ -0,0 +1,46 @@
+from supabase import create_client, Client
+import pandas as pd
+import string
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from IPython.display import Image
+from textblob import TextBlob
+from PIL import Image
+
+# Replace with your actual values
+SUPABASE_URL = "https://bvtirlkozgihvapfzovn.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2dGlybGtvemdpaHZhcGZ6b3ZuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDE5NDY5NCwiZXhwIjoyMDU5NzcwNjk0fQ.i2NCQCBiE6vzQ8rZbAHSxewjrfuOsYCE28J2xdtnwA8"
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+
+response = supabase.table("dup_customer_interactions_duplicate").select("*").execute()
+
+#print("Data:", response.data)
+df = pd.DataFrame(response.data)
+print(df)
+df2 = pd.DataFrame({'transcript': [], 'Sentiment': []})
+
+
+for index, row in df.iterrows():
+    text =row['transcript']
+    lower_case = text.lower()
+    lower_case = lower_case.replace('-', ' ')
+    cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
+    score = SentimentIntensityAnalyzer().polarity_scores(cleaned_text)
+    if score['neg'] > score['pos']:
+        stt = "Negative Sentiment"
+        scr =score['pos']-score['neg']
+    elif score['neg'] < score['pos']:
+        stt = "Positive Sentiment"
+        scr = score['pos'] - score['neg']
+    else:
+        stt = "Neutral Sentiment"
+        scr = score['pos'] - score['neg']
+    update_response = supabase.table("dup_customer_interactions_duplicate").update({
+        "sentiment_score": scr, "sentiment": stt
+    }).eq("id", row["id"]).execute()
+
+
+
